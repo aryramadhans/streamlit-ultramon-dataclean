017f5c7ef84b44e0715ec2ef2b77c7d35d4bc4cb
aryramadhans <aryramadhans@gmail.com>
2025-06-18 15:05:38 +0700

    Modify Home Description

--- dashboard_view.py
+++ dashboard_view.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 def display_home_page():
-    st.title("Welcome to the Data Cleaning Dashboard")
-    st.write("Use the sidebar to navigate to the Data Cleaning page or other sections.")
+    st.title("Ultramon Data Cleaning Dashboard")
     st.image("img/Telkom Indonesia.png", width=500)
     st.markdown("### Features:")
-    st.write("- Upload and clean your CSV files")
-    st.write("- Preview the cleaned data")
-    st.write("- Download the cleaned data")
+    st.write("1. Upload files to clean your data")
+    st.write("2. Preview the cleaned data")
+    st.write("3. Download the cleaned data")
     st.write("Start by navigating to **Data Cleaning** in the sidebar.")
